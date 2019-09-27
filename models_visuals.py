b12cff3fd0859ebca0cbb5bf0ff2d102d7935a71
Ian Reinert <ian.reinert@gmail.com>
2019-09-27 11:32:31 -0400

    UPDATE technical notebook and models_visuals

--- models_visuals.py
+++ models_visuals.py
@@ -7,15 +7,13 @@ from sklearn.model_selection import train_test_split
 from yellowbrick.classifier import ConfusionMatrix
 
 
+
 def split_data(data, target):
     """splits data"""
     X = data.drop(columns=target)
     y = data[target]
     X_train, X_test, y_train, y_test = train_test_split(X, y)
     return X_train, X_test, y_train, y_test
-
-
-X_train, X_test, y_train, y_test = split_data(data, target)
 
 
 def list_important_features(data, best_forest):
@@ -25,7 +23,7 @@ def list_important_features(data, best_forest):
     return features.sort_values(by=0, ascending=False)
 
 
-def roc_visual(best_model):
+def roc_visual(best_model, X_train, X_test, y_test):
     """Creates ROC visual"""
     y_train_pred = best_model.predict(X_train)
     probs_test = best_model.predict_proba(X_test)[:, 1]
@@ -49,12 +47,11 @@ def roc_visual(best_model):
     plt.show()
 
 
-def nice_confusion(model):
+def nice_confusion(model, X_train, X_test, y_train, y_test):
     """Creates a nice looking confusion matrix"""
     plt.figure(figsize=(10, 10))
     plt.xlabel('Predicted Class', fontsize=18)
     plt.ylabel('True Class', fontsize=18)
-    plt.xticks(labels=[''])
     viz = ConfusionMatrix(
         model,
         cmap='PuBu', fontsize=18)
@@ -63,18 +60,18 @@ def nice_confusion(model):
     viz.poof()
 
 
-def baseline_logistic():
+def baseline_logistic(X_train, X_test, y_train, y_test):
     """Runs baseline logistic regression model"""
     regr = LogisticRegression(C=1e5, solver='liblinear')
     regr.fit(X_train, y_train)
     y_train_pred = regr.predict(X_train)
-    nice_confusion(regr)
+    nice_confusion(regr, X_train, X_test, y_train, y_test)
     y_test_pred = regr.predict(X_test)
     print("Baseline logistic regression classification report: \n", classification_report(y_test, y_test_pred))
     return regr
 
 
-def visual_metrics(model):
+def visual_metrics(model, X_train, X_test, y_train, y_test):
     """Creates confusion and ROC curve"""
-    nice_confusion(model)
-    roc_visual(model)
+    nice_confusion(model, X_train, X_test, y_train, y_test)
+    roc_visual(model, X_train, X_test)
